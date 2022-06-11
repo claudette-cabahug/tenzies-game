7629931c0dec6a23aa618fd6ed396292eac13f76
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-06-12 00:34:53 +1200

    create a function that would return a new array of 10 random numbers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import './App.css'
 import Die from "./Die"
 
 export default function App() {
+  function allNewDice() {
+    const newDiceArr = []
+    for(let i = 0; i < 10; i++) {
+      let randomNumber = Math.floor(Math.random() * 6)
+      newDiceArr.push(randomNumber)
+    }
+
+    return newDiceArr
+  }
+  console.log(allNewDice())
+
   return (
     <main>
       <div className="dice-container">
