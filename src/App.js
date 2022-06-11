4010ab7f43c01d48286b5918fdd6e0a7c71be716
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-06-12 01:11:58 +1200

    create the roll button and the function to handle the change on click added styling to the button and spacing/font to the dice

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css'
 import Die from "./Die"
 
 export default function App() {
+  // 2
   const [dice, setDice] = React.useState(allNewDice())
 
+  // 1
   function allNewDice() {
     const newDiceArr = []
     for(let i = 0; i < 10; i++) {
@@ -15,13 +17,20 @@ export default function App() {
     return newDiceArr
   }
 
+  // 3
   const diceElements = dice.map(die => <Die value={die} />)
+
+  // 4
+  function rollDice() {
+    setDice(allNewDice())
+  }
 
   return (
     <main>
       <div className="dice-container">
         {diceElements}
       </div>
+      <button className="roll-dice" onClick={rollDice}>Roll</button>
     </main>
   )
 }
