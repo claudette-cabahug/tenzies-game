9b5bd70f9224419833744b234ce0c25819fb99ad
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-06-12 12:48:22 +1200

    install nanoid to be able to generate a unique key prop and add the key as object prop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import './App.css'
 import Die from "./Die"
-// import { nanoid } from "nanoid"
+import { nanoid } from "nanoid"
 
 export default function App() {
   // 2
@@ -15,7 +15,7 @@ export default function App() {
       newDiceArr.push({
         value: randomNumber,
         isHeld: false,
-        // id: nanoid()
+        id: nanoid()
       })
     }
 
@@ -23,7 +23,9 @@ export default function App() {
   }
 
   // 3
-  const diceElements = dice.map(die => <Die value={die.value} />)
+  const diceElements = dice.map(die => (
+    <Die key={die.id} value={die.value} />
+    ))
 
   // 4
   function rollDice() {
