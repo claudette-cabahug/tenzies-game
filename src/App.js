c3b170ffb7f53745914cf7e6cbf73e8fb6949b0b
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-06-12 16:35:41 +1200

    install Confetti library and conditionally render it if tenzies is true, meaning the user has won the game

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import './App.css'
 import Die from "./Die"
 import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App() {
 
@@ -59,6 +60,7 @@ export default function App() {
 
   return (
     <main>
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
